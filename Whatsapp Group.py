ff06c672fd043ace706524ec5eca1ab2a579d953
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-12-10 19:31:04 +0200

    Update Whatsapp Group.py

--- Whatsapp Group.py
+++ Whatsapp Group.py
@@ -16,19 +16,22 @@ driver.get("https://web.whatsapp.com")
 
 
 new_sheet = "Group" + str(datetime.now())
-filename = r'contact.xlsx'
+filename = r'contact.xlsx' # your execl name must be exatcly contact.xlsx.
 df = pd.read_excel(filename)
-names = list(df['name'])
+# names = List[str] with all contacts that you want to add
+names = list(df['name']) # you must have column 'name' in your execl. all the contacts you want to add must appear there, as they saved in your whatsapp.
 
-time_now = str(datetime.today())[0:16]
+# some shity way to create new excel with details about success\failure to add contact to group
+time_now = str(datetime.today())[0:16] 
 time_now_fixed = time_now.replace(":", ".")
 status_sheet = "status " + time_now_fixed + ".xlsx"
 workbook = xlsxwriter.Workbook(status_sheet)
 sheet = workbook.add_worksheet("Status")
 
-
+#go to whatsapp web then scan the QR and press anything
 input("Enter anything after scanning QR code")
 
+#click on the menu in order to open new group. your whatsapp ,must be in hebrew else change the placeholder.
 menu_bar = driver.find_element_by_xpath('//div[@role="button"][@title="תפריט"]')
 menu_bar.click()
 sleep(randint(1,2))
@@ -36,6 +39,7 @@ new_group = driver.find_element_by_xpath('//div[@role="button"][@title="קבוצ
 new_group.click()
 sleep(randint(1,2))
 
+# Searching for the contact then click to add him\her to the group, clear the search bar and keep going to the next name.
 row = 0
 for i in names:
     try:
